bf970a46b16fc1a5e34d2b472618d7a02dbfbcb8
yd <3034008462@qq.com>
2019-11-15 16:19:11 +0800

    提交项目框架

---
+++ entity/src/main/java/model/LoginModel.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * 2019/11/15,Create by yaodan
+ */
+public class LoginModel {
+    private String name;
+}
---
+++ mall/src/main/java/com/cn/wanxi/mall/Pinpai.java
@@ -0,0 +1,15 @@
+package com.cn.wanxi.mall;
+
+import java.util.List;
+
+/**
+ * 2019/11/15,Create by yaodan
+ */
+
+public class Pinpai {
+
+    public List<String> getList(){
+        return null;
+    }
+
+}
